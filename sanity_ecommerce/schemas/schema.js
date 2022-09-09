4e7761c9b2d29d8d5ceb549ca94e567349719592
linusatpug <playmathematicsinstitution.pmi@gmail.com>
2022-09-09 22:27:27 +0545

    updating files in github

--- sanity_ecommerce/schemas/schema.js
+++ sanity_ecommerce/schemas/schema.js
@@ -1,11 +1,9 @@
-// First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from 'part:@sanity/base/schema-creator';
+import schemaTypes from 'all:part:@sanity/base/schema-type';
+import product from './product';
+import banner from './banner';
 
-// Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
-
-// Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   name: 'default',
-  types: schemaTypes.concat([]),
+  types: schemaTypes.concat([product, banner]),
 })
