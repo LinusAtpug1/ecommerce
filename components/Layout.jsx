9f2cb4c30f0a71ab539e7f132944e40457acaa40
linusatpug <playmathematicsinstitution.pmi@gmail.com>
2022-09-15 22:33:33 +0545

    recent commit

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
-const Layout = () => {
+const Layout = ({children}) => {
   return (
-    <div>
-      
+    <div className="layout">
+      <Head>
+        <title>Linus Mart</title>
+      </Head>
+
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">
+        {children}
+
+      </main>
+
+      <footer>
+        <Footer/>
+      </footer>
+
     </div>
   )
 }
