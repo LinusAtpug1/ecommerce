d8e39b993b1a498902aeee4f6748f81e4f0f9d2a
linusatpug <playmathematicsinstitution.pmi@gmail.com>
2022-09-17 22:26:46 +0545

    next

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,13 +1,15 @@
 import React from 'react'
 import {client, urlFor} from '../../lib/client';
 
-const ProductDetails = () => {
+const ProductDetails = ({products, product}) => {
+  const {image, name, details, price} = product;
+
   return (
     <div>
       <div className="product-detail-container">
         <div>
             <div className="image-container">
-                <img src=""/>
+                <img src={urlFor(image && image[0])}/>
 
             </div>
         </div>
@@ -15,14 +17,38 @@ const ProductDetails = () => {
     </div>
   )
 }
-export const getStaticProps = async ({params: {slug}}) => {
-    const query = '*[_type== "product"]';
+  export const getStaticPaths = async () => {
+    const query = `*[_type == "product"] {
+      slug {
+        current
+      }
+    }`
+
     const products = await client.fetch(query);
+    const paths = products.map((product) => ({
+      params: {
+        slug: product.slug.current
+      }
+    }));
+
+    return {
+      paths,
+      fallback: 'blocking'
+    }
+  }
+
+
+  export const getStaticProps = async ({params: {slug}}) => {
+    const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+    const productsQuery = '*[_type == "product"]'
+
+
+    const product = await client.fetch(query);
+    const products = await client.fetch(productsQuery);
   
-    const bannerQuery = '*[_type== "banner"]';
-    const bannerData = await client.fetch(bannerQuery);
+    
     return{
-      props: { products, bannerData }
+      props: { product, products }
     }
   
   }
